ef7f6361a10e87f7dd8682837c4c45f3fb0c126f
Jenn <jenniferjuarezjj@gmail.com>
2022-10-19 05:12:05 -0400

    create useState for cash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-
 import React from 'react'
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import './styles/App.css'
@@ -11,6 +11,9 @@ import BurgerShop from './Components/BurgerShop/BurgerShop'
 import SuperMarket from './Components/SuperMarket/SuperMarket'
 
 const App = () => {
+  const [cash, setCash] = useState(100)
+
+
   return (
     <main>
       <Nav />
